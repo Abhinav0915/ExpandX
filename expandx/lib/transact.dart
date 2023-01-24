41ac4bd1b4a635efa5e6e7d40a648fe503c9e726
Abhinav Saxena <abhinav150601@gmail.com>
2023-01-24 19:10:46 +0530

    final edits

--- expandx/lib/transact.dart
+++ expandx/lib/transact.dart
@@ -63,66 +63,231 @@ class _transactState extends State<transact> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: MyAppBar(),
-        bottomNavigationBar: BottomNavigationBar(
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(Icons.business),
-              label: 'Transact',
+      resizeToAvoidBottomInset: false,
+      appBar: MyAppBar(),
+      body: SafeArea(
+        child: Column(children: <Widget>[
+          Container(
+            margin: EdgeInsets.fromLTRB(12, 15, 12, 0),
+            height: 100.0,
+            width: 290.0,
+            decoration: BoxDecoration(
+              color: Color.fromARGB(255, 238, 183, 179),
+              borderRadius: BorderRadius.circular(10.0),
+              border: Border.all(
+                color: Colors.black,
+              ),
             ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.dashboard),
-              label: 'Dashboard',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.person),
-              label: 'Account',
-            ),
-          ],
-          currentIndex: 0,
-          selectedItemColor: Color.fromARGB(255, 216, 114, 74),
-          unselectedItemColor: Colors.grey,
-          showUnselectedLabels: true,
-          onTap: (index) {
-            if (index == 1) {
-              Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(builder: (context) => const savetax()),
-              );
-            } else if (index == 2) {
-              Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(builder: (context) => const fund()),
-              );
-            }
-          },
-        ),
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.fromLTRB(30, 12, 10, 0),
-            child: Column(
-              children: [
-                Container(
-                    margin: EdgeInsets.fromLTRB(18, 10, 10, 0),
-                    height: 100.0,
-                    width: 320.0,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10.0),
-                        color: Color.fromARGB(255, 245, 207, 151)),
-                    child: Positioned(
-                      top: 30.0,
-                      left: 20.0,
-                      child: const Text(
-                        "sdsd",
-                      ),
-                    )),
-
-              ],
-              
-            ),
-            
           ),
-        ));
+          SizedBox(
+            height: 20,
+          ),
+          Container(
+              margin: EdgeInsets.fromLTRB(0, 0, 260, 0),
+              child: const Text(
+                "Popular Funds",
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              )),
+          Row(
+            children: [
+              Container(
+                margin: const EdgeInsets.fromLTRB(10, 10, 0, 0),
+                width: MediaQuery.of(context).size.width * 0.93,
+                height: 105,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(color: Colors.white),
+                  borderRadius: BorderRadius.circular(5),
+                  boxShadow: const [
+                    BoxShadow(
+                      color: Colors.grey,
+                      blurRadius: 1.0,
+                      spreadRadius: 1.0,
+                      offset: Offset(0.0, 0.0),
+                    )
+                  ],
+                ),
+                child: Row(
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.fromLTRB(00, 0, 0, 40),
+                      child: Image.asset(
+                        "assets/images/kotak.png",
+                        width: 50,
+                        height: 50,
+                      ),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.fromLTRB(0, 0, 0, 40),
+                      child: const Expanded(
+                        child: Text(
+                            " Mirae Asset Emergging - \n  Bluechip Fund - Growth"),
+                      ),
+                    ),
+                    Column(
+                      children: const [
+                        Padding(
+                          padding: EdgeInsets.fromLTRB(110, 14, 0, 0),
+                          child: Text("12.46%"),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.fromLTRB(90, 0, 4, 30),
+                          child: Text("> 5Y returns"),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Container(
+              margin: EdgeInsets.fromLTRB(0, 0, 250, 0),
+              child: const Text(
+                "Best Categories",
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              )),
+          Row(
+            children: <Widget>[
+              Container(
+                margin: const EdgeInsets.fromLTRB(10, 10, 0, 0),
+                height: MediaQuery.of(context).size.height * 0.35,
+                width: MediaQuery.of(context).size.width * 0.93,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(5),
+                  boxShadow: const [
+                    BoxShadow(
+                      color: Colors.grey,
+                      blurRadius: 1.0,
+                      spreadRadius: 1.0,
+                      offset: Offset(0.0, 0.0),
+                    )
+                  ],
+                ),
+                child: Row(
+                  children: [
+                    Container(
+                      height: 100.0,
+                      width: 100.0,
+                      margin: const EdgeInsets.fromLTRB(10, 0, 0, 120),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10),
+                        boxShadow: const [
+                          BoxShadow(
+                            color: Colors.grey,
+                            spreadRadius: 1.0,
+                            offset: Offset(2.0, 2.0),
+                          )
+                        ],
+                      ),
+                      child: Image.asset(
+                        "assets/images/img1.jpg",
+                        width: 50,
+                        height: 50,
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 8,
+                    ),
+                    Container(
+                      height: 100.0,
+                      width: 100.0,
+                      margin: const EdgeInsets.fromLTRB(10, 0, 0, 120),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10),
+                        boxShadow: const [
+                          BoxShadow(
+                            color: Colors.grey,
+                            spreadRadius: 1.0,
+                            offset: Offset(2.0, 2.0),
+                          )
+                        ],
+                      ),
+                      child: Image.asset(
+                        "assets/images/img2.jpg",
+                        width: 50,
+                        height: 50,
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    Container(
+                      height: 100.0,
+                      width: 100.0,
+                      margin: const EdgeInsets.fromLTRB(10, 0, 0, 120),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10),
+                        boxShadow: const [
+                          BoxShadow(
+                            color: Colors.grey,
+                            spreadRadius: 1.0,
+                            offset: Offset(2.0, 2.0),
+                          )
+                        ],
+                      ),
+                      child: Image.asset(
+                        "assets/images/img3.png",
+                        width: 50,
+                        height: 50,
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ]),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Transact',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.dashboard),
+            label: 'Dashboard',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Account',
+          ),
+        ],
+        currentIndex: 0,
+        selectedItemColor: const Color.fromARGB(255, 216, 114, 74),
+        unselectedItemColor: Colors.grey,
+        showUnselectedLabels: true,
+        onTap: (index) {
+          if (index == 1) {
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(builder: (context) => const savetax()),
+            );
+          } else if (index == 2) {
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(builder: (context) => const fund()),
+            );
+          }
+        },
+      ),
+    );
   }
 }
 
@@ -130,91 +295,3 @@ class _transactState extends State<transact> {
 
 
 
-// SafeArea(
-//           child: Column(children: [
-//             SizedBox(
-//               height: 25.0,
-//               width: MediaQuery.of(context).size.height * 0.6,
-//             ),
-//             Container(
-//               width: 360,
-//               height: 125,
-//               decoration: BoxDecoration(
-//                 color: Color.fromARGB(255, 255, 214, 160),
-//                 borderRadius: BorderRadius.circular(10),
-//                 border: Border.all(
-//                   color: Color.fromARGB(255, 235, 175, 135),
-//                   width: 1,
-//                 ),
-//                 boxShadow: [
-//                   BoxShadow(
-//                     color: Colors.grey.withOpacity(0.5),
-//                     spreadRadius: 1,
-//                     blurRadius: 1,
-//                     offset: const Offset(0, 1),
-//                   ),
-//                 ],
-//               ),
-//               child: Column(
-//                 children: const [
-//                   Padding(
-//                     padding: EdgeInsets.fromLTRB(0, 18, 105, 0),
-//                     child: Text(
-//                       "Get Started With Ease!",
-//                       style: TextStyle(
-//                         fontSize: 20,
-//                         fontWeight: FontWeight.bold,
-//                       ),
-//                     ),
-//                   ),
-//                   Padding(
-//                     padding: EdgeInsets.fromLTRB(10, 25, 90, 0),
-//                     child: Text(
-//                       "Make a smart move towards a better financial future",
-//                       style: TextStyle(
-//                         fontSize: 15,
-//                       ),
-//                     ),
-//                   ),
-//                 ],
-//               ),
-//             ),
-//             const SizedBox(
-//               height: 20.0,
-//             ),
-//             SizedBox(z
-//               child: Row(
-//                 children: [
-//                   Container(
-//                     margin: EdgeInsets.fromLTRB(10, 0, 0, 0),
-//                     width: 500,
-//                     height: 350,
-//                     decoration: const BoxDecoration(
-//                       color: Colors.white,
-//                     ),
-//                     child: const Text("Popular Funds",
-//                         style: TextStyle(
-//                           fontSize: 20,
-//                           fontWeight: FontWeight.bold,
-//                         )),
-//                   ),
-//                   Container(
-//                     width: 20.0,
-//                   ),
-//                   Column(
-//                     children: [
-//                       Container(
-//                         width: 20,
-//                         height: 20,
-//                         decoration: const BoxDecoration(
-//                           color: Colors.white,
-//                         ),
-//                         child: const Icon(Icons.arrow_forward_ios),
-//                       ),
-//                     ],
-//                   )
-//                 ],
-//               ),
-//             )
-//           ]),
-//         )
