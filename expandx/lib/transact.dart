84bbefe130893fa1f6280a126eb6836dada46bcc
Abhinav Saxena <abhinav150601@gmail.com>
2023-01-24 20:15:27 +0530

    final edits

--- expandx/lib/transact.dart
+++ expandx/lib/transact.dart
@@ -65,194 +65,262 @@ class _transactState extends State<transact> {
     return Scaffold(
       resizeToAvoidBottomInset: false,
       appBar: MyAppBar(),
-      body: SafeArea(
-        child: Column(children: <Widget>[
-          Container(
-            margin: EdgeInsets.fromLTRB(12, 15, 12, 0),
-            height: 100.0,
-            width: 290.0,
-            decoration: BoxDecoration(
-              color: Color.fromARGB(255, 238, 183, 179),
-              borderRadius: BorderRadius.circular(10.0),
-              border: Border.all(
-                color: Colors.black,
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Column(children: <Widget>[
+            Container(
+              margin: EdgeInsets.fromLTRB(12, 15, 12, 0),
+              height: 100.0,
+              width: 290.0,
+              decoration: BoxDecoration(
+                color: Color.fromARGB(255, 238, 183, 179),
+                borderRadius: BorderRadius.circular(10.0),
+                border: Border.all(
+                  color: Colors.black,
+                ),
               ),
             ),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Container(
-              margin: EdgeInsets.fromLTRB(0, 0, 260, 0),
-              child: const Text(
-                "Popular Funds",
-                textAlign: TextAlign.left,
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-              )),
-          Row(
-            children: [
-              Container(
-                margin: const EdgeInsets.fromLTRB(10, 10, 0, 0),
-                width: MediaQuery.of(context).size.width * 0.93,
-                height: 105,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  border: Border.all(color: Colors.white),
-                  borderRadius: BorderRadius.circular(5),
-                  boxShadow: const [
-                    BoxShadow(
-                      color: Colors.grey,
-                      blurRadius: 1.0,
-                      spreadRadius: 1.0,
-                      offset: Offset(0.0, 0.0),
-                    )
-                  ],
-                ),
-                child: Row(
-                  children: [
-                    Container(
-                      margin: const EdgeInsets.fromLTRB(00, 0, 0, 40),
-                      child: Image.asset(
-                        "assets/images/kotak.png",
-                        width: 50,
-                        height: 50,
-                      ),
-                    ),
-                    Container(
-                      margin: const EdgeInsets.fromLTRB(0, 0, 0, 40),
-                      child: const Expanded(
-                        child: Text(
-                            " Mirae Asset Emergging - \n  Bluechip Fund - Growth"),
-                      ),
-                    ),
-                    Column(
-                      children: const [
-                        Padding(
-                          padding: EdgeInsets.fromLTRB(110, 14, 0, 0),
-                          child: Text("12.46%"),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+                margin: EdgeInsets.fromLTRB(0, 0, 260, 0),
+                child: const Text(
+                  "Popular Funds",
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                )),
+            Row(
+              children: [
+                Container(
+                  margin: const EdgeInsets.fromLTRB(10, 10, 0, 0),
+                  width: MediaQuery.of(context).size.width * 0.93,
+                  height: 105,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    border: Border.all(color: Colors.white),
+                    borderRadius: BorderRadius.circular(5),
+                    boxShadow: const [
+                      BoxShadow(
+                        color: Colors.grey,
+                        blurRadius: 1.0,
+                        spreadRadius: 1.0,
+                        offset: Offset(0.0, 0.0),
+                      )
+                    ],
+                  ),
+                  child: Row(
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.fromLTRB(00, 0, 0, 40),
+                        child: Image.asset(
+                          "assets/images/kotak.png",
+                          width: 50,
+                          height: 50,
                         ),
-                        Padding(
-                          padding: EdgeInsets.fromLTRB(90, 0, 4, 30),
-                          child: Text("> 5Y returns"),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.fromLTRB(0, 0, 0, 40),
+                        child: const Expanded(
+                          child: Text(
+                              " Mirae Asset Emergging - \n  Bluechip Fund - Growth"),
+                        ),
+                      ),
+                      Column(
+                        children: const [
+                          Padding(
+                            padding: EdgeInsets.fromLTRB(110, 14, 0, 0),
+                            child: Text("12.46%"),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.fromLTRB(90, 0, 4, 30),
+                            child: Text("> 5Y returns"),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+                margin: EdgeInsets.fromLTRB(0, 0, 250, 0),
+                child: const Text(
+                  "Best Categories",
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                )),
+            Row(
+              children: <Widget>[
+                Container(
+                  margin: const EdgeInsets.fromLTRB(10, 10, 0, 0),
+                  height: MediaQuery.of(context).size.height * 0.35,
+                  width: MediaQuery.of(context).size.width * 0.93,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(5),
+                    // boxShadow: const [
+                    //   BoxShadow(
+                    //     color: Colors.grey,
+                    //     blurRadius: 1.0,
+                    //     spreadRadius: 1.0,
+                    //     offset: Offset(0.0, 0.0),
+                    //   )
+                    // ],
+                  ),
+                  child: Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Container(
+                              height: 100.0,
+                              width: 100.0,
+                              // margin: const EdgeInsets.fromLTRB(10, 0, 0, 120),
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                border: Border.all(color: Colors.white),
+                                borderRadius: BorderRadius.circular(10),
+                                boxShadow: const [
+                                  BoxShadow(
+                                    // color: Colors.grey,
+                                    spreadRadius: 1.0,
+                                    offset: Offset(2.0, 2.0),
+                                  )
+                                ],
+                              ),
+                              child: Image.asset(
+                                "assets/images/img1.jpg",
+                                width: 50,
+                                height: 50,
+                              ),
+                            ),
+                            const SizedBox(
+                              width: 8,
+                            ),
+                            Container(
+                              height: 100.0,
+                              width: 100.0,
+                              // margin: const EdgeInsets.fromLTRB(10, 0, 0, 120),
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(10),
+                                boxShadow: const [
+                                  BoxShadow(
+                                    color: Colors.grey,
+                                    spreadRadius: 1.0,
+                                    offset: Offset(2.0, 2.0),
+                                  )
+                                ],
+                              ),
+                              child: Image.asset(
+                                "assets/images/img2.jpg",
+                                width: 50,
+                                height: 50,
+                              ),
+                            ),
+                            const SizedBox(
+                              width: 10,
+                            ),
+                            Container(
+                              height: 100.0,
+                              width: 100.0,
+                              // margin: const EdgeInsets.fromLTRB(10, 0, 0, 120),
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(10),
+                                boxShadow: const [
+                                  BoxShadow(
+                                    color: Colors.grey,
+                                    spreadRadius: 1.0,
+                                    offset: Offset(2.0, 2.0),
+                                  )
+                                ],
+                              ),
+                              child: Image.asset(
+                                "assets/images/img3.png",
+                                width: 50,
+                                height: 50,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Container(
+                              margin: const EdgeInsets.fromLTRB(10, 20, 0, 0),
+                              child: Container(
+                                height: 100.0,
+                                width: 100.0,
+                                // margin: const EdgeInsets.fromLTRB(10, 0, 0, 120),
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(10),
+                                  boxShadow: const [
+                                    BoxShadow(
+                                      color: Colors.grey,
+                                      spreadRadius: 1.0,
+                                      offset: Offset(2.0, 2.0),
+                                    )
+                                  ],
+                                ),
+                                child: Image.asset(
+                                  "assets/images/img4.jpeg",
+                                  width: 50,
+                                  height: 50,
+                                ),
+                              ),
+                            ),
+                            const SizedBox(
+                              width: 10,
+                            ),
+                            Container(
+                              margin: const EdgeInsets.fromLTRB(10, 20, 0, 0),
+                              child: Container(
+                                height: 100.0,
+                                width: 100.0,
+                                // margin: const EdgeInsets.fromLTRB(10, 0, 0, 120),
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(10),
+                                  boxShadow: const [
+                                    BoxShadow(
+                                      color: Colors.grey,
+                                      spreadRadius: 1.0,
+                                      offset: Offset(2.0, 2.0),
+                                    )
+                                  ],
+                                ),
+                                child: Image.asset(
+                                  "assets/images/img4.jpeg",
+                                  width: 50,
+                                  height: 50,
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
                       ],
                     ),
-                  ],
+                  ),
                 ),
-              )
-            ],
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Container(
-              margin: EdgeInsets.fromLTRB(0, 0, 250, 0),
-              child: const Text(
-                "Best Categories",
-                textAlign: TextAlign.left,
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-              )),
-          Row(
-            children: <Widget>[
-              Container(
-                margin: const EdgeInsets.fromLTRB(10, 10, 0, 0),
-                height: MediaQuery.of(context).size.height * 0.35,
-                width: MediaQuery.of(context).size.width * 0.93,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(5),
-                  boxShadow: const [
-                    BoxShadow(
-                      color: Colors.grey,
-                      blurRadius: 1.0,
-                      spreadRadius: 1.0,
-                      offset: Offset(0.0, 0.0),
-                    )
-                  ],
-                ),
-                child: Row(
-                  children: [
-                    Container(
-                      height: 100.0,
-                      width: 100.0,
-                      margin: const EdgeInsets.fromLTRB(10, 0, 0, 120),
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(10),
-                        boxShadow: const [
-                          BoxShadow(
-                            color: Colors.grey,
-                            spreadRadius: 1.0,
-                            offset: Offset(2.0, 2.0),
-                          )
-                        ],
-                      ),
-                      child: Image.asset(
-                        "assets/images/img1.jpg",
-                        width: 50,
-                        height: 50,
-                      ),
-                    ),
-                    const SizedBox(
-                      width: 8,
-                    ),
-                    Container(
-                      height: 100.0,
-                      width: 100.0,
-                      margin: const EdgeInsets.fromLTRB(10, 0, 0, 120),
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(10),
-                        boxShadow: const [
-                          BoxShadow(
-                            color: Colors.grey,
-                            spreadRadius: 1.0,
-                            offset: Offset(2.0, 2.0),
-                          )
-                        ],
-                      ),
-                      child: Image.asset(
-                        "assets/images/img2.jpg",
-                        width: 50,
-                        height: 50,
-                      ),
-                    ),
-                    const SizedBox(
-                      width: 10,
-                    ),
-                    Container(
-                      height: 100.0,
-                      width: 100.0,
-                      margin: const EdgeInsets.fromLTRB(10, 0, 0, 120),
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(10),
-                        boxShadow: const [
-                          BoxShadow(
-                            color: Colors.grey,
-                            spreadRadius: 1.0,
-                            offset: Offset(2.0, 2.0),
-                          )
-                        ],
-                      ),
-                      child: Image.asset(
-                        "assets/images/img3.png",
-                        width: 50,
-                        height: 50,
-                      ),
-                    ),
-                  ],
-                ),
-              )
-            ],
-          ),
-        ]),
+              ],
+            ),
+          ]),
+        ),
       ),
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
@@ -290,8 +358,3 @@ class _transactState extends State<transact> {
     );
   }
 }
-
-
-
-
-
